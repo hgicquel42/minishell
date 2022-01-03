286ff847e4861faab9e7d57a5b5ce36a569d67cc
hgicquel <hgicquel@student.42.fr>
2022-01-03 16:26:17 +0100

    it works

--- split.c
+++ split.c
@@ -6,38 +6,62 @@
 /*   By: hgicquel <hgicquel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/03 13:28:36 by hgicquel          #+#    #+#             */
-/*   Updated: 2022/01/03 14:02:46 by hgicquel         ###   ########.fr       */
+/*   Updated: 2022/01/03 16:25:14 by hgicquel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "main.h"
 
+static int	ft_count_quote(char *s, int *r)
+{
+	int	j;
+
+	j = 0;
+	while (s[j + 1] && s[j + 1] != '"')
+		j++;
+	*r = j + 2;
+	return (j);
+}
+
+static int	ft_count_raw(char *s, int *r, char c)
+{
+	int	j;
+
+	j = 0;
+	while (s[j] && s[j] != c)
+		j++;
+	*r = j;
+	return (j);
+}
+
+static int	ft_count(char *s, int *r, char c)
+{
+	if (*s == '"')
+		return (ft_count_quote(s, r));
+	else
+		return (ft_count_raw(s, r, c));
+}
+
 static	int	ft_split_count(char *s, char c)
 {
-	size_t	j;
-	size_t	l;
+	int	r;
+	int	l;
 
 	l = 0;
 	while (*s)
 	{
 		while (*s && *s == c)
 			s++;
-		j = 0;
-		while (*s && *s != c)
-		{
-			j++;
-			s++;
-		}
-		if (j)
+		if (ft_count(s, &r, c))
 			l++;
-		// s += j;
+		s += r;
 	}
 	return (l);
 }
 
-static	int	ft_split_free(char **r, size_t k)
+static	int	ft_split_free(char **r, int k)
 {
-	size_t	i;
+	int	i;
 
 	i = 0 ;
 	while (i < k)
@@ -46,11 +70,13 @@ static	int	ft_split_free(char **r, size_t k)
 	return (0);
 }
 
-static	int	ft_split_alloc(char *s, char c, char **r)
+static	int	ft_split_alloc(char *s, char c, char **p)
 {
-	size_t	i;
-	size_t	j;
-	size_t	k;
+	int	i;
+	int	i2;
+	int	j;
+	int	k;
+	int	r;
 	char	*w;
 
 	k = 0;
@@ -58,29 +84,30 @@ static	int	ft_split_alloc(char *s, char c, char **r)
 	{
 		while (*s && *s == c)
 			s++;
-		j = 0;
-		while (*s && *s != c)
-		{
-			j++;
-			s++;
-		}
+		j = ft_count(s, &r, c);
 		if (!j)
 			continue ;
 		w = malloc(j + 1);
 		if (!w)
-			return (ft_split_free(r, k));
+			return (ft_split_free(p, k));
 		i = 0;
-		while (i < j)
-			w[i++] = *(s++);
+		i2 = 0;
+		while (i < r)
+		{
+			if (s[i] != '"')
+				w[i2++] = s[i];
+			i++;
+		}
+		s += i;
 		w[j] = 0;
-		r[k++] = w;
+		p[k++] = w;
 	}
 	return (1);
 }
 
 char	**ft_split(char *s, char c)
 {
-	size_t	l;
+	int		l;
 	char	**r;
 
 	l = ft_split_count(s, c);
