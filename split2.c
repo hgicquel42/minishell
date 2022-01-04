cf2b3c3792495c4141de42f6180f7da9db312bb0
hgicquel <hgicquel@student.42.fr>
2022-01-04 14:39:56 +0100

    single quote

--- split2.c
+++ split2.c
@@ -6,11 +6,17 @@
 /*   By: hgicquel <hgicquel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/03 17:10:06 by hgicquel          #+#    #+#             */
-/*   Updated: 2022/01/03 17:13:42 by hgicquel         ###   ########.fr       */
+/*   Updated: 2022/01/04 14:39:09 by hgicquel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "main.h"
+
+void	ft_noop(int i, int j)
+{
+	(void)i;
+	(void)j;
+}
 
 int	ft_split_count(char *s, int *r, char c)
 {
@@ -21,20 +27,40 @@ int	ft_split_count(char *s, int *r, char c)
 	j = 0;
 	while (s[i] && s[i] != c)
 	{
-		if (s[i] != '"')
+		if (s[i] == '"')
 		{
 			i++;
-			j++;
+			while (s[i])
+			{
+				if (s[i] == '"')
+					break ;
+				else if (s[i] == '$')
+				{
+					
+				}
+				else
+				{
+					i++;
+					j++;
+				}
+				
+			}
+			i++;
 		}
-		else
+		else if (s[i] == '\'')
 		{
 			i++;
-			while (s[i] && s[i] != '"')
+			while (s[i] && s[i] != '\'')
 			{
 				i++;
 				j++;
 			}
 			i++;
+		}
+		else
+		{
+			i++;
+			j++;
 		}
 	}
 	*r = i;
@@ -50,14 +76,21 @@ void	ft_split_copy(char *s, char *w, char c)
 	j = 0;
 	while (s[i] && s[i] != c)
 	{
-		if (s[i] != '"')
-			w[j++] = s[i++];
-		else
+		if (s[i] == '"')
 		{
 			i++;
 			while (s[i] && s[i] != '"')
 				w[j++] = s[i++];
 			i++;
 		}
+		else if (s[i] == '\'')
+		{
+			i++;
+			while (s[i] && s[i] != '\'')
+				w[j++] = s[i++];
+			i++;
+		}
+		else
+			w[j++] = s[i++];
 	}
 }
